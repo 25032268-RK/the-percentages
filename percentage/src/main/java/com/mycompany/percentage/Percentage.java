552bdf11b581d7b82be65e99fa2c6c2dd42249f2
25032268-RK <25032268@mvula.univen.ac.za>
2026-08-19 15:22:31 +0200

    Update Percentage.java

--- percentage/src/main/java/com/mycompany/percentage/Percentage.java
+++ percentage/src/main/java/com/mycompany/percentage/Percentage.java
@@ -20,18 +20,15 @@ public class Percentage {
     }
     
     public static void main(String[] args) {
-     
-        
-        System.out.println("Enter the first value: "); 
+
        double num1 = 2.0;
-        System.out.println("Enter the second value: ");
        double num2 = 5.0;
        
        double result = computePercent(num1, num2);
-        System.out.println(num1 + "is " + result + " percent of " + num2);
+        System.out.println(num1 + " is " + result + " percent of " + num2);
         
        double result2 = computePercent(num2, num1);
-        System.out.println(num2 + "is " + result2 + " percent of " + num1 );
+        System.out.println(num2 + " is " + result2 + " percent of " + num1 );
     }
   
 }
